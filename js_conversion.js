c468412422435ddde162fbca2f3f3baf06cbf657
rajveer doriya <rajveerdoriyaofficial@gmail.com>
2024-08-12 22:00:43 +0530

    conversion

---
+++ js_conversion.js
@@ -0,0 +1,80 @@
+/*
+let age=33;
+
+
+console.log(typeof age);
+console.log(typeof (age));
+
+//-----------
+
+let marks="33";
+
+console.log(typeof marks); // string
+
+//string convert into number
+
+let convertIntoNumber=Number(marks);
+
+console.log(typeof convertIntoNumber);//number
+
+let bame="33ds";
+let vame=null;
+
+let convert=Number(bame);
+let convert2=Number(vame);
+
+console.log(typeof convert);//number 
+console.log(convert);//number [but nan]
+console.log(convert2);//null [0]
+*/
+
+
+//---------------------
+
+
+/*
+let rajveer=1;
+
+//convert into boolean
+let boolean=Boolean(rajveer);
+
+console.log(typeof boolean);//boolean
+console.log(boolean);//true
+
+
+//next
+
+let doriya=352;
+
+//convert into boolean
+let boolean2=Boolean(doriya);
+
+console.log(typeof boolean2);//boolean
+console.log(boolean2);//true
+
+//next
+
+let hello="raj";
+
+//convert into boolean
+let boolean3=Boolean(hello);
+
+console.log(typeof boolean3);//boolean
+console.log(boolean3);//true
+*/
+
+//convert number into string 
+
+let value=33;
+let nuberIntoString=String(value);
+
+console.log(typeof nuberIntoString)//string
+console.log(nuberIntoString);//33 [but in string ]
+
+let value2=true;
+let nuberIntoString2=String(value2);
+
+console.log(typeof nuberIntoString2)//string
+console.log(nuberIntoString2);//33 [but in string ]
+
+
