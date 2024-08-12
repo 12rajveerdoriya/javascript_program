4029bde5f65e90fe5397aa2aa5082ac14b13a2c5
rajveer doriya <rajveerdoriyaofficial@gmail.com>
2024-08-12 22:29:02 +0530

    summary

---
+++ js_datatype_summary.js
@@ -0,0 +1,32 @@
+//primitive datatype
+/*7 type
+
+1.string
+2.number
+3.undefined
+4.null
+5.boolean
+6.symbol  [give a unique id ]
+7.bigInt [ add n in the end of numbers ]
+
+*/
+//--------------------------------------------
+
+//bigInt
+
+let number=675325436n;//[ add n in the end of numbers ]
+console.log(typeof number);//bigInt
+//sybol example
+const value=Symbol(`123`);
+const value2=Symbol(`123`);
+console.log(value===value2)
+
+//non primitive (refrence)
+
+/*
+
+1.array
+2.object 
+3.function
+
+*/
