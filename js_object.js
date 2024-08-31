b71ef526b5bdad74b7428b386517a62bbb0b560f
RAJVEER DORIYA <rajveerdoriyaofficial@gmail.com>
2024-08-31 14:00:25 +0530

    object

--- js_object.js
+++ js_object.js
@@ -11,16 +11,31 @@ const man={
    
 }
  
-console.log(typeof man["name"]);
-console.log(typeof man[mysym]);
+// console.log(typeof man["name"]);
+// console.log(typeof man[mysym]);
 
 //change object values 
 
 man.age=21;
 
 //freeze object values after than no change 
-Object.freeze(man)
-man.age=23;
-console.log(man);
+// Object.freeze(man);
+// man.age=23;
+// console.log(man);
+
+
+man.greeting =function(){
+   console.log("hello my name is rajveer ")
+}
+console.log(man.greeting());
+man.greetingTwo =function(){
+   console.log(`hello my name is ${this.name}`)
+}
+console.log(man.greetingTwo());
+
+
+
+
+
 
 
