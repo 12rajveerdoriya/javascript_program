386d2182a6fc3909b2c43e8b9a41ae31248eccbd
RAJVEER DORIYA <rajveerdoriyaofficial@gmail.com>
2024-09-09 13:46:08 +0530

    object example

---
+++ js_object2.js
@@ -0,0 +1,13 @@
+//singleton object declare 
+// let obj=new Object();
+// console.log(obj);//{} --show object 
+
+const obj2={}
+// console.log(obj2);//{}
+obj2.hii="hello";
+obj2.why="hello";
+console.log(obj2);
+console.log(obj2.hii);
+
+
+
